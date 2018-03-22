1955e9875d6e46a77db6854de8fcdf42a853df51
kashkama <owen.kamau2014@gmail.com>
2018-03-22 23:54:18 +0300

    adding sorting logic

--- src/lib/sort-helper.js
+++ src/lib/sort-helper.js
@@ -1,2 +1,15 @@
+const sortBy = (type, field) => {
+    switch(type) {
+        case "date":
+            return (a,b) => new Date(b[field]) - new Date(a[field]);
+        default:
+            return (a,b) => b[field] - a[field];
+    }
+};
+
 export const sortFunction = sort => 
-    (sort === "SORT_BY_")
+    (sort === "SORT_BY_DATE") ?
+        sortBy("date", "timestamp") :
+        (sort === "SORTED_BY_PRICE") ?
+            sortBy("number", "price") :
+            sortBy("number", "rating");
